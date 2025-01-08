3e4defe7e7232ad0c03ca4bfe05f26543efa2c11
rok1337 <facugiovanella88@gmail.com>
2025-01-07 23:15:52 -0300

    practica de spring boot

---
+++ src/test/java/example/cashcard/CashCardJsonTest.java
@@ -0,0 +1,36 @@
+package example.cashcard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.boot.test.json.JacksonTester;
+
+import java.io.IOException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@JsonTest
+class CashCardJsonTest {
+
+    @Autowired
+    private JacksonTester<CashCard> json;
+
+    @Test
+    void cashCardDeserializationTest() throws IOException {
+        String expected = """
+            {
+                "id":99,
+                "amount":123.45
+            }
+            """;
+        assertThat(json.parse(expected))
+            .isEqualTo(new CashCard(1000L, 67.89));
+        assertThat(json.parseObject(expected).id()).isEqualTo(1000);
+        assertThat(json.parseObject(expected).amount()).isEqualTo(67.89);
+        assertThat(json.parse(expected))
+            .isEqualTo(new CashCard(99L, 123.45));
+        assertThat(json.parseObject(expected).id()).isEqualTo(99);
+        assertThat(json.parseObject(expected).amount()).isEqualTo(123.45);
+    }
+}
+
