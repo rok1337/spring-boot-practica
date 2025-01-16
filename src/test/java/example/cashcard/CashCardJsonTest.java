b47f4375571cd07da8c08461f906536261491d4a
rok1337 <facugiovanella88@gmail.com>
2025-01-16 16:27:33 -0300

    Check

--- src/test/java/example/cashcard/CashCardJsonTest.java
+++ src/test/java/example/cashcard/CashCardJsonTest.java
@@ -1,20 +1,33 @@
 package example.cashcard;
 
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.JsonTest;
 import org.springframework.boot.test.json.JacksonTester;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @JsonTest
 class CashCardJsonTest {
-
-    @Autowired
     private JacksonTester<CashCard> json;
+    private JacksonTester<List<CashCard>> jsonList;
+    private List<CashCard> cashCards;
+
+    @BeforeEach
+    void setUp() {
+        cashCards = Arrays.asList(
+            new CashCard(99L, 123.45),
+            new CashCard(100L, 100.00),
+            new CashCard(101L, 150.00),
+            new CashCard(100L, 1.00)
+        );
+    }
+
 
     @Test
     void cashCardSerializationTest() throws IOException {
@@ -41,4 +54,22 @@ class CashCardJsonTest {
         assertThat(json.parseObject(expected).id()).isEqualTo(99);
         assertThat(json.parseObject(expected).amount()).isEqualTo(123.45);
     }
+
+    @Test
+    void cashCardListSerializationTest() throws IOException {
+        assertThat(jsonList.write(cashCards)).isStrictlyEqualToJson("list.json");
+    }
+
+    @Test
+    void cashCardListDeserializationTest() throws IOException {
+        String expected="""
+            [
+                { "id": 99, "amount": 123.45 },
+                { "id": 100, "amount": 100.00 },
+                { "id": 101, "amount": 150.00 }
+            ]
+            """;
+            assertThat(jsonList.parse(expected)).isEqualTo(cashCards);
+        }
+
 }
--- src/test/java/example/cashcard/CashcardApplicationTests.java
+++ src/test/java/example/cashcard/CashcardApplicationTests.java
@@ -2,6 +2,7 @@ package example.cashcard;
 
 import com.jayway.jsonpath.DocumentContext;
 import com.jayway.jsonpath.JsonPath;
+import net.minidev.json.JSONArray;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,6 +14,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.net.URI;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+//@DirtiesContext(classMode = ClassMode.AFTER_EACH_TEST_METHOD)
 class CashCardApplicationTests {
     @Autowired
     TestRestTemplate restTemplate;
@@ -30,6 +32,9 @@ class CashCardApplicationTests {
 
         Double amount = documentContext.read("$.amount");
         assertThat(amount).isEqualTo(123.45);
+
+        assertThat(id).isNotNull();
+        assertThat(amount).isEqualTo(250.00);
     }
 
     @Test
@@ -42,7 +47,7 @@ class CashCardApplicationTests {
 
     @Test
     void shouldCreateANewCashCard() {
-        CashCard newCashCard = new CashCard(null, 250.00);
+        CashCard newCashCard = new CashCard(44L, 250.00);
         ResponseEntity<Void> createResponse = restTemplate.postForEntity("/cashcards", newCashCard, Void.class);
         assertThat(createResponse.getStatusCode()).isEqualTo(HttpStatus.CREATED);
 
@@ -50,6 +55,47 @@ class CashCardApplicationTests {
         ResponseEntity<String> getResponse = restTemplate.getForEntity(locationOfNewCashCard, String.class);
         assertThat(getResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
     }
+    @Test
+    void shouldReturnAllCashCardsWhenListIsRequested() {
+        ResponseEntity<String> response = restTemplate.getForEntity("/cashcards", String.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+   
+        DocumentContext documentContext = JsonPath.parse(response.getBody());
+        int cashCardCount = documentContext.read("$.length()");
+        assertThat(cashCardCount).isEqualTo(3);
+   
+        JSONArray ids = documentContext.read("$..id");
+        assertThat(ids).containsExactlyInAnyOrder(99, 100, 101);
+   
+        JSONArray amounts = documentContext.read("$..amount");
+        assertThat(amounts).containsExactlyInAnyOrder(123.45, 100.0, 150.00);
+    }
 
+    
+
+
+
+    @Test
+    void shouldReturnAPageOfCashCards() {
+    ResponseEntity<String> response = restTemplate.getForEntity("/cashcards?page=0&size=1", String.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+        DocumentContext documentContext = JsonPath.parse(response.getBody());
+        JSONArray page = documentContext.read("$[*]");
+        assertThat(page.size()).isEqualTo(1);
+    }
+
+    @Test
+void shouldReturnASortedPageOfCashCardsWithNoParametersAndUseDefaultValues() {
+    ResponseEntity<String> response = restTemplate.getForEntity("/cashcards", String.class);
+    assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+    DocumentContext documentContext = JsonPath.parse(response.getBody());
+    JSONArray page = documentContext.read("$[*]");
+    assertThat(page.size()).isEqualTo(3);
+
+    JSONArray amounts = documentContext.read("$..amount");
+    assertThat(amounts).containsExactly(1.00, 123.45, 150.00);
+}
 
 }
